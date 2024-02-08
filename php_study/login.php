1b76dfd35dff231d324b7912c53a231c159c31f4
Asrul Hidayat <136725199+AsrulHidayat@users.noreply.github.com>
2024-02-08 09:42:24 +0800

    Update login and register

--- php_study/login.php
+++ php_study/login.php
@@ -1,10 +1,48 @@
 <?php 
     include "service/database.php";
+
+    // syarat untuk menggunakan session
+    session_start();
+
+    // is_login : ketika sudah login maka kita akan tetap di arahkan langsung ke dashboard tanpa harus melakukan login kembali.
+    if(isset($_SESSION["is_login"])){
+        header("location: dashboard.php");
+    }
+
+    // diguankan untuk pengecualian saj ketika akun tidak ditamukan dan terisi dengan value baru
+    $login_message = "";
     
     // isset berfungsi untuk memvalidasi apakah data tersebut atau sesuai dengan benar
     if(isset($_POST['login'])){
         $username = $_POST['username'];
         $password = $_POST['password'];
+
+        // pengeksekusian query SQL
+        $sql = "SELECT *  FROM users WHERE username = '$username' AND password = '$password'";
+
+        // hasil eksekusian query SQL
+        $result = mysqli_query($db, $sql);
+
+        // jika hasilnya memiliki num_row (data di dalam database) lebih dari nol, maka muncul "data ada".
+        if($result->num_rows > 0){
+                // echo "datanya ada";
+
+            //fetch_assoc : bawaan dari sananya
+            $data = $result->fetch_assoc();
+
+            // SESSION : Tempat untuk data ditampung
+            $_SESSION["username"] = $data["username"];
+            $_SESSION["is_login"] = true;
+
+            header("location: dashboard.php");
+
+            // digunakan hanya untuk testing saja
+                // echo "data username adalah : " . $data["username"];
+                // echo "data password adalah : " . $data["password"];
+
+        } else {
+            $login_message = "akun tidak ditemukan";
+        }
     }
 ?>
 
@@ -19,11 +57,13 @@
     <?php include "layout/header.html" ?>
 
     <h3>MASUK AKUN</h3>
+    <i><?= $login_message ?></i>
 
     <!-- POST berfungsi untuk mengirim data ke sistem atau di balik layar -->
     <form action="login.php" method="POST">
         <input type="text" placeholder="username" name="username"/>
         <input type="password" placeholder="password" name="password"/>
+
         <!-- Fungsi "name" disini ialah memberikan validasi ketika di klik -->
         <button type="submit" name="login">Masuk sekarang</button>
     </form>
--- php_study/register.php
+++ php_study/register.php
@@ -1,3 +1,38 @@
+<?php
+    include "service/database.php";
+    session_start();
+
+    $register_message = "";
+
+    if(isset($_SESSION["is_login"])){
+        header("location: dashboard.php");
+    }
+
+    if(isset($_POST["register"])) {
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+
+        // menyamarkan password yang ada
+        $hash_password = hash("sha256", $password);
+
+        try {
+            // sql adalah untuk memberikan koneksi ke dalam data mySQL (menggunakan query)
+            $sql = "INSERT INTO users (username, password) VALUES ('$username', '$hash_password');";
+
+            if($db->query($sql)) {
+                // echo "OK MANTAP DATA MASUK";
+                $register_message = "daftar akun berhasil, silahkan login";
+            } else {
+                // echo "DATA GAGAL MASUK";
+                $register_message = "username sudah ada, silahkan ganti";
+            }
+        
+        } catch (mysqli_sql_exception $e) {
+            $register_message = "username sudah ada, silahkan ganti";
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,11 +43,14 @@
 <body>
     <?php include "layout/header.html" ?>
     <h3>DAFTAR AKUN</h3>
-    <form>
+    <i><?= $register_message ?></i>
+
+    <form action="register.php" method="POST">
         <input type="text" placeholder="username" name="username"/>
         <input type="password" placeholder="password" name="password"/>
-        <button type="submit">daftar sekarang</button>
+        <button type="submit" name="register">daftar sekarang</button>
     </form>
+
     <?php include "layout/footer.html" ?>
 </body>
 </html>
